057c8d719fc129a1c8c8c2ce953d5f4b8111440e
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-14 18:04:23 -0500

    2 files modified

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,4 @@
 
 <div class="app">
 	<Navbar />
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -2,8 +2,24 @@
 	import './styles.css';
 </script>
 
-<div class="w-full h-12 bg-slate-900">
-  <a href="">create</a>
-  <a href="">pricing</a>
-  <a href="">about</a>
-</div>
+<nav class="flex items-center justify-between flex-wrap bg-slate-900 p-6">
+  <div class="flex items-center flex-shrink-0 text-slate-100 mr-6">
+    <span class="font-semibold text-xl tracking-tight">Skald</span>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-slate-100 hover:text-white mr-4">
+        Create
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-slate-100 hover:text-white mr-4">
+        Pricing
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-slate-100 hover:text-white">
+        About
+      </a>
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0">Sign in</a>
+    </div>
+  </div>
+</nav>
