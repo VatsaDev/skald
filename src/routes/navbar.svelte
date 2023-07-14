5248e59f72bebcfa6730a1d63c91af95238b1029
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-14 18:20:55 -0500

    1 file modified, 2 added, 2 deleted

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-slate-900 p-6">
   <div class="flex items-center flex-shrink-0 text-slate-100 mr-6">
-    <img src="../lib/images/logo-white.png" alt="logo-white">
+    <img src="/favicon.png" alt="logo-white">
     <span class="font-semibold text-4xl tracking-tight">Skald</span>
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
@@ -17,7 +17,7 @@
       </a>
       <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-slate-100 hover:text-white hover:underline">
         About
-      </a>
+      </a> 
     </div>
     <div>
       <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0">Sign in</a>
