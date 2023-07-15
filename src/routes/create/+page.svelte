8b6fb0bc209dd27f45162842a97ee44c177edd29
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 10:56:42 -0500

    1 file modified

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -3,6 +3,11 @@
 	<meta name="description" content="create" />
 </svelte:head>
 
+<script type="module">
+    import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.4.1';
+</script>
+
 <section>
 	<h1 class="text-center text-8xl m-8">Create</h1>
+	<input class="m-8 border p-2" type="text" placeholder="hello">
 </section>
