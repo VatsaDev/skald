2416ad71bbb18c48ce4cf4820376f5ce573f965a
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 09:02:01 -0500

    2 files added

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Skald sign in</title>
+	<meta name="description" content="sign-in" />
+</svelte:head>
+
+<section>
+	<div class="w-4/5 h-96 mx-auto my-8 p-6 shadow-xl bg-slate-100 rounded-lg">
+    <h1 class="text-center text-2xl font-bold">Sign in</h1>
+  </div>	
+</section>
