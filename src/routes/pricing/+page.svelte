ddd2b27f82c54401bbd111237c8f2d2e817ad95c
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 10:48:24 -0500

    1 file modified

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -4,5 +4,28 @@
 </svelte:head>
 
 <section>
-	<h1 class="text-center text-8xl m-8">pricing</h1>
+	<h1 class="text-center text-8xl m-8">Pricing</h1>
+  <div class="flex flex-col lg:flex-row w-5/6 mx-auto text-white">
+    <div class="w-full lg:w-1/3 bg-slate-900 m-2 p-2 rounded-lg">
+      <h1 class="text-center text-4xl m-8">Village Plan</h1>
+      <h1 class="text-center text-2xl m-8">Free</h1>
+      <ul class="list-disc list-inside m-8">
+        <li>100 tokens/month</li>  
+      </ul>
+    </div>
+    <div class="w-full lg:w-1/3 bg-slate-900 m-2 p-2 rounded-lg">
+      <h1 class="text-center text-4xl m-8">Traveller Plan</h1>
+      <h1 class="text-center text-2xl m-8">9.99/month</h1>
+      <ul class="list-disc list-inside m-8">
+        <li>1000 tokens/month</li>  
+      </ul>
+    </div>
+    <div class="w-full lg:w-1/3 bg-slate-900 m-2 p-2 rounded-lg">
+      <h1 class="text-center text-4xl m-8">Royal Plan</h1>
+      <h1 class="text-center text-2xl m-8">19.99/month</h1>
+      <ul class="list-disc list-inside m-8">
+        <li>10000 tokens/month</li>  
+      </ul>
+    </div>
+  </div>
 </section>
