35c85f4a19f83e97e9574dfdbe6f0c87a5c89a63
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 20:06:52 -0500

    1 file modified

--- src/routes/create/+page.js
+++ src/routes/create/+page.js
@@ -1,4 +1,4 @@
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.4.1';
+/*import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.4.1';
 
 let poet = await pipeline('text2text-generation', 'Xenova/LaMini-Flan-T5-783M');
 let result = await poet('Write me a love poem about cheese.', {
@@ -11,4 +11,4 @@ let result = await poet('Write me a love poem about cheese.', {
 	// do_sample: true,
 });
 
-console.log(result);
+console.log(result);*/
