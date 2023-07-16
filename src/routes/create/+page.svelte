c67adf6fd9a01b54b5a5f4a080b06de134d835ba
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 20:42:51 -0500

    1 file modified

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -4,20 +4,21 @@
 </svelte:head>
 
 <script>
-	import fetch from "node-fetch";
+	//import fetch from "node-fetch";
 	async function query(data) {
-    const response = await fetch(
-        "https://api-inference.huggingface.co/models/gpt2",
-        {
-            headers: { Authorization: `Bearer ${API_TOKEN}` },
-            method: "POST",
-            body: JSON.stringify(data),
-        }
-    );
-    const result = await response.json();
-    return result;
-}
-query("Can you please let us know more details about your ").then((response) => {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/gpt2",
+            {
+                headers: { Authorization: `Bearer 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` },
+                method: "POST",
+                body: JSON.stringify(data),
+            }
+        );
+        const result = await response.json();
+        return result;
+    }
+    
+    query("Can you please let us know more details about your ").then((response) => {
     console.log(JSON.stringify(response));
 });
 </script>
