b6179aeeb1645e538b80aa5330cb5d62aba1acb0
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-15 20:22:07 -0500

    1 file modified, 1 deleted

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -3,6 +3,25 @@
 	<meta name="description" content="create" />
 </svelte:head>
 
+<script>
+	import fetch from "node-fetch";
+	async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/gpt2",
+        {
+            headers: { Authorization: `Bearer ${API_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+query("Can you please let us know more details about your ").then((response) => {
+    console.log(JSON.stringify(response));
+});
+</script>
+
 <section>
 	<h1 class="text-center text-8xl m-8">Create</h1>
 	<input class="m-8 border p-2" type="text" placeholder="hello">
