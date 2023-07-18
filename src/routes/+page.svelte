dda85b5c3e9b2b225e0cbf163ec9885de7ada0d3
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-18 17:34:56 -0500

    1 file modified, 1 added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,18 +7,29 @@
 </svelte:head>
 
 <section>
-	<img src="/logo-black.png" alt="logo-black" class="mx-auto mt-6"><br>
-	<h1 class="font-sans font-bold text-8xl text-center my-2">
-		Skald
-	</h1>
-	<h2 class="text-center text-2xl my-4">
-		Unique, Evocative Stories with the power of <span class="text-amber-500 font-bold underline decoration-4">Generative AI</span>, at your fingertips
-	</h2>
-	<h2 class="text-center text-xl m-8">Make your own stories, as <span class="text-amber-500 font-bold">amazing</span> as they can be, with <span class="text-amber-500 font-bold">Powerful AI Models</span> </h2>
-	<h2 class="text-center text-xl my-4">Make your stories, as <span class="text-amber-500 font-bold">descriptive</span> as they can be, with <span class="text-amber-500 font-bold">Word Suggestions</span> </h2>
-  <h2 class="text-center text-xl my-4">Don't like it? get a <span class="text-amber-500 font-bold">Full Rewrite</span></h2>
-	<h2 class="text-center text-xl my-4">Tailor your stories, by <span class="text-amber-500 font-bold">Age group</span> and <span class="text-amber-500 font-bold">Diverse Story Types</span> </h2>
-	
+	<header>
+		<img src="/logo-black.png" alt="logo-black" class="mx-auto mt-6"><br>
+		<h1 class="font-sans font-bold text-8xl text-center my-2">
+			Skald
+		</h1>
+		<h2 class="text-center text-4xl my-4">
+			Lose yourself <br>
+			<div class="text-center text-2xl my-4">
+				in the northern mines
+			</div>
+		</h2>
+		<h2 class="text-center text-lg my-4">
+			Unique, Evocative Stories with the power of Generative AI, at your fingertips
+		</h2>
+	</header>
+
+	<div class="grid grid-cols-2">
+		<h2 class="text-center text-xl m-8">Make your own stories, as <span class="text-amber-500 font-bold">amazing</span> as they can be, with <span class="text-amber-500 font-bold">Powerful AI Models</span> </h2>
+		<!--<img src="https://cdn.arstechnica.net/wp-content/uploads/2023/05/google_palm2_hero_1-800x450.jpg" alt="palm" class="w-3/4 mx-auto drop-shadow-xl">-->
+		<h2 class="text-center text-xl my-4">Make your stories, as <span class="text-amber-500 font-bold">descriptive</span> as they can be, with <span class="text-amber-500 font-bold">Word Suggestions</span> </h2>
+  	<h2 class="text-center text-xl my-4">Don't like it? get a <span class="text-amber-500 font-bold">Full Rewrite</span></h2>
+		<h2 class="text-center text-xl my-4">Tailor your stories, by <span class="text-amber-500 font-bold">Age group</span> and <span class="text-amber-500 font-bold">Diverse Story Types</span> </h2>
+	</div>	
 	<footer class="my-8 mx-2 invisible lg:visible">
 		<p class="font-mono">Copyright Vatsa 2023-present, all rights reserved</p>
 	</footer>
